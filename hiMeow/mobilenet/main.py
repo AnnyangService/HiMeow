13551c7279083f6b323362637c79082fe5ab5c70
JoJunHwa <jjj5306@naver.com>
2024-10-27 00:55:37 +0900

    feat : modify main function path

--- hiMeow/mobilenet/main.py
+++ hiMeow/mobilenet/main.py
@@ -1,5 +1,18 @@
 import torch
-from dataloader.dataLoader import CatEyeDatasetCustomized, data_transforms
+import sys
+import os
+
+current_file = os.path.abspath(__file__)
+current_dir = os.path.dirname(current_file)
+project_root = os.path.dirname(os.path.dirname(current_dir))
+
+if project_root not in sys.path:
+    sys.path.append(project_root)
+
+for path in sys.path:
+    print(path)
+
+from hiMeow.mobilenet.dataloader.dataLoader import CatEyeDatasetCustomized, data_transforms
 from hiMeow.mobilenet.utils.utils import train_or_load_models
 
 
@@ -7,7 +20,11 @@ def main():
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print(f"Using device: {device}")
 
-    dataset = CatEyeDatasetCustomized('../../dataset/Training', transform=data_transforms)
+    # 데이터셋 경로 설정
+    dataset_path = os.path.join(project_root, 'dataset', 'Training')
+    print(f"Dataset path: {dataset_path}")
+
+    dataset = CatEyeDatasetCustomized(dataset_path, transform=data_transforms)
     print(f"Total samples: {len(dataset)}")
 
     diseases = ['각막궤양', '결막염', '안검염', '백내장', '녹내장']
@@ -17,6 +34,7 @@ def main():
 
     disease_models = train_or_load_models(diseases, dataset, device, use_k_fold=use_k_fold, k=k)
     print("Model loading/training complete for all diseases!")
+
 
 if __name__ == '__main__':
     main()
